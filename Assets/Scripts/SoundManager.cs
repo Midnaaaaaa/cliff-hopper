e75e57bacbcde14d52d7137903a4d97aa2003a86
Midnaaaaaa <ijuaagi@gmail.com>
2023-06-03 23:54:26 +0200

    musica finarda 🎧 + ui

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -11,6 +11,7 @@ public class SoundManager : MonoBehaviour
     public static SoundManager Instance { get; private set; }
 
 
+
     private void Awake()
     {
         if (Instance != null && Instance != this)
